e5452fa8fa64cf01222f4470f81eb3d3d15cf9e3
matteo-cristino <cristino.matteo97@gmail.com>
2024-12-03 10:45:18 +0100

    fix: allow also authorization header

--- server.go
+++ server.go
@@ -17,7 +17,7 @@ import (
 func proxyHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 	if r.Method == "OPTIONS" {
 		return
